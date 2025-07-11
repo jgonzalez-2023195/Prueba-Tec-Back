bd63e2c831ff389ff653f24c9f76ee11f2f4e6a3
jgonzalez-2023195 <jgonzalez-2023195@kinal.edu.gt>
2025-07-11 16:14:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { config } from "dotenv";
-import { dbConnection } from "./configs/db.js";
+
 import ExpressServer from "./configs/server.js";
 
 config();
-dbConnection();
+
 const server = new ExpressServer();
 server.listen();
